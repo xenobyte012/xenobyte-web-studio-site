4de4822bf319ecaffc4e6972f9c74678ec775f85
Clement Mukhale <xenobyte012@gmail.com>
2026-05-17 21:56:51 +0200

    updating the website to run on the domain

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,9 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/xenobyte-web-studio-site">
+
+    <BrowserRouter >
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+
 );
